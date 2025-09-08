970e683f71e23466201379636e41f309e3c43634
Aleksandr Ponomarev <ax.dev@yahoo.com>
2025-09-08 23:14:21 +0300

    do

--- Chapter03-vscode/IterationsStatements/Program.cs
+++ Chapter03-vscode/IterationsStatements/Program.cs
@@ -8,3 +8,14 @@ while (x < 10)
     WriteLine(x);
     x++;
 }
+
+string? password;
+
+do
+{
+    Write("Enter your password:  ");
+    password = ReadLine();
+}
+while (password != "Pa$$w0rd");
+
+WriteLine("Correct!");
