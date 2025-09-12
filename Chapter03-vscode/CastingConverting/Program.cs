77f5c25e1f45b77295c98248f788ecfb2fe3821f
Aleksandr Ponomarev <ax.dev@yahoo.com>
2025-09-12 19:20:18 +0300

    Type conversion

---
+++ Chapter03-vscode/CastingConverting/Program.cs
@@ -0,0 +1,11 @@
+﻿//
+
+using static System.Console;
+
+int a = 10;
+double b = a; // Implicit conversion from int to double
+WriteLine(b);
+
+double c = 9.8;
+int d = (int)c;
+WriteLine(d); // Explicit conversion from double to int (fractional part is truncated)
