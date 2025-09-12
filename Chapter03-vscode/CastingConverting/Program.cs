5c080a40c6a4c50abd290f3d2709b332e4162590
Aleksandr Ponomarev <ax.dev@yahoo.com>
2025-09-12 19:41:24 +0300

    MidpointRounding.AwayFromZero

--- Chapter03-vscode/CastingConverting/Program.cs
+++ Chapter03-vscode/CastingConverting/Program.cs
@@ -8,3 +8,11 @@ foreach (double dbl in doubles)
 {
     WriteLine($"{dbl} to int 32 is {ToInt32(dbl)}");
 }
+
+foreach (double dbl in doubles)
+{
+    WriteLine(format:
+    "Math.Round({0}, 0, MidpointRounding.AwayFromZero) is {1}",
+    arg0: dbl,
+    arg1: Math.Round(value: dbl, digits: 0, mode: MidpointRounding.AwayFromZero));
+}
