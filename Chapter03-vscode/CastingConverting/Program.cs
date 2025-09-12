e30b3a597bc022f333450abe0e38a5ae65df8718
Aleksandr Ponomarev <ax.dev@yahoo.com>
2025-09-12 19:23:01 +0300

    unsafety conversation

--- Chapter03-vscode/CastingConverting/Program.cs
+++ Chapter03-vscode/CastingConverting/Program.cs
@@ -9,3 +9,15 @@ WriteLine(b);
 double c = 9.8;
 int d = (int)c;
 WriteLine(d); // Explicit conversion from double to int (fractional part is truncated)
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}"); // Explicit conversion from long to int
+
+e = long.MaxValue;
+f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
+
+e = 5_000_000_000;
+f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
