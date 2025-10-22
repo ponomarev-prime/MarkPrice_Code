b66cd90a92b3dfeaed9e2cc59016aa5a8ff7f800
Aleksandr Ponomarev <ax.dev@yahoo.com>
2025-10-22 23:03:40 +0300

    byte i

---
+++ ex32/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int max = 500;
+
+for (byte i = 0; i < max; i++)
+{
+    Console.WriteLine(i);
+}
+
+// так как byte = 8 бит, переполнение по модулю 256
